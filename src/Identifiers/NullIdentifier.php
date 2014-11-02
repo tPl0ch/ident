88104834844d18343c95677c5ec5ba979b866836
Thomas Ploch <thomas.ploch@meinfernbus.de>
2014-11-02 13:19:24 +0100

    Added `NullIdentifier`

---
+++ src/Identifiers/NullIdentifier.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Ident\Identifiers;
+
+use Ident\Exception\IdentExceptions;
+use Ident\IdentifiesObjects;
+
+/**
+ * Class NullIdentifier
+ */
+final class NullIdentifier implements IdentifiesObjects
+{
+    /**
+     * @param mixed $signature
+     *
+     * @return IdentifiesObjects
+     *
+     * @throws \Ident\Exception\InvalidSignature
+     */
+    public static function fromSignature($signature)
+    {
+        if ($signature !== null) {
+            throw IdentExceptions::invalidSignature('NOT NULL');
+        }
+
+        return new self();
+    }
+
+    /**
+     * This method returns a unique identity representation
+     *
+     * @return mixed
+     */
+    public function signature()
+    {
+        return null;
+    }
+
+    /**
+     * @param IdentifiesObjects $id
+     *
+     * @return bool
+     */
+    public function equals(IdentifiesObjects $id)
+    {
+        return $id instanceof self;
+    }
+}
